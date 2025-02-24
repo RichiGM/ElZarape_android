ddd9594f7ab681b53f7a4bc7878a7c08bb928538
RichiGM <garnicamejiaricardo@gmail.com>
2025-02-24 08:20:38 -0600

    CAMBIO DE PANTALLA

--- app/src/main/java/com/baite/elzarape/view/ActivityAlimentos.java
+++ app/src/main/java/com/baite/elzarape/view/ActivityAlimentos.java
@@ -1,4 +1,14 @@
 package com.baite.elzarape.view;
 
-public class ActivityAlimentos {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.baite.elzarape.R;
+
+public class ActivityAlimentos extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alimentos);
+        // ... your code here
+    }
 }

82509f0cc7f0e62d3fed3e70eb12d6f96db0c80f
dannabuendia <dannabuendia564@gmail.com>
2025-02-24 07:38:23 -0600

    Nada

---
+++ app/src/main/java/com/baite/elzarape/commons/ZarapeCommons.java
@@ -0,0 +1,12 @@
+package com.baite.elzarape.commons;
+
+public class ZarapeCommons {
+
+    public static final String SERVERURL = "http://10.16.8.55:8080/ElZarape2";
+    public static final String TCREDENCIAL = "{\"nombre\"%s\",:\"Danna\",\"password\":\"12345\"}";
+
+
+
+
+
+}
